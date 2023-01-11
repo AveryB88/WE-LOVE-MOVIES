3c1471857b2f40b3686adc93b18f098c7f71457f
Avery B <94032280+AveryB88@users.noreply.github.com>
2023-01-10 22:42:53 -0600

    Add files via upload

---
+++ Project-_WeLoveMovies-_Qualified-Avery_Bowen-Solution/src/errors/notFound.js
@@ -0,0 +1,10 @@
+function notFound(req, res, next) {
+    const message = {
+        status: 404,
+        message: `Path not found: ${req.originalUrl}`
+    }
+    next(message);
+}
+
+
+module.exports = notFound
